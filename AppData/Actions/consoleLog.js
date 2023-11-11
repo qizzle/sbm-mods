a7b300874e3adae5bde199def5ea9120a420c401
qizzle <dimitribartscha71@gmail.com>
2023-11-11 18:57:20 +0100

    ConsoleLog Mod

--- AppData/Actions/consoleLog.js
+++ AppData/Actions/consoleLog.js
@@ -14,17 +14,10 @@ module.exports = {
       storeAs: "log",
       name: "Input to log",
     },
-
-    {
-      element: "input",
-      storeAs: "nothing",
-      name: "Autoupdater showcase",
-    },
   ],
 
   async run(values, interaction, client, bridge) {
     const log = bridge.transf(values.log);
-    // Add something to showcase autoupdater
     console.log(log);
   },
 };
