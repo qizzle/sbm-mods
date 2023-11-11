d1e12a23ca95ec6964507b1bb82606feaf3245ac
qizzle <dimitribartscha71@gmail.com>
2023-11-11 18:29:14 +0100

    add Console log mod (demo for autoupdater)

---
+++ AppData/Actions/consoleLog.js
@@ -0,0 +1,24 @@
+/*
+  Console Log mod by qschnitzel
+  Licensed under MIT License
+
+  Log something to the bots console.
+*/
+module.exports = {
+  data: {
+    name: "Console Log",
+  },
+  UI: [
+    {
+      element: "input",
+      storeAs: "log",
+      name: "Input to log",
+    },
+  ],
+
+  async run(values, interaction, client, bridge) {
+    const log = bridge.transf(values.log);
+
+    console.log(log);
+  },
+};
